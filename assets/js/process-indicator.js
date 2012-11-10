a06c9ef12f05b7e01b01c6d08a26839140b18184
piecioshka <piecioshka@gmail.com>
2012-11-10 17:30:41 +0100

    fix

--- assets/js/process-indicator.js
+++ assets/js/process-indicator.js
@@ -4,11 +4,28 @@
   // master scope
   var global = this;
 
-  global.process_indicator = {
-    hide_play_bar: function () {
-      $(".play-bar").hide();
-    },
+  // aktualna liczba procentow loadera
+  var current_percent_progress = 0;
 
+  function can_change(new_percent) {
+    return new_percent > current_percent_progress && new_percent < 100;
+  }
+
+  function update_progress_view(percent) {
+    $(".videos-wrapper .progress .bar").animate({
+      width: percent + "%"
+    });
+  }
+
+  function update_progress(percent) {
+    // console.log("percent", percent);
+    if (can_change(percent)) {
+      update_progress_view(percent);
+      current_percent_progress = percent;
+    }
+  }
+
+  global.process_indicator = {
     show_play_bar: function () {
       $(".play-bar").fadeIn();
     },
@@ -25,19 +42,12 @@
       $(".videos-wrapper .progress").remove();
     },
 
-    grow_loading: function (val, max) {
-      var percent = get_percent_value_of(val, max);
-      if (percent <= 100) {
-        $(".videos-wrapper .progress .bar").animate({
-          width: percent + "%"
-        });
-      }
+    grow_loading_percent_value: function (val, max) {
+      update_progress(get_percent_value_of(val, max));
     },
 
     grow_loading_bar: function () {
-      $(".videos-wrapper .progress .bar").animate({
-        width: "+=5%"
-      });
+      update_progress(current_percent_progress + 5);
     }
   };
 }).call(this);
