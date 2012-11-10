1c23e934b61360bed31b2c643e73148933dfc5fd
piecioshka <piecioshka@gmail.com>
2012-11-10 12:17:27 +0100

    console addons

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,7 @@
 //= require lib/jquery-1.8.2.js
 //= require lib/bootstrap.js
 //= require lib/socket.io.js
+//= require utils.js
 //= require game.js
 //= require player.manager.js
 //= require player.js
