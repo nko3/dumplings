d12fccc45e6db229ea95fc31a7c1f12266c4aa23
piecioshka <piecioshka@gmail.com>
2012-11-10 07:51:59 +0100

    fell the bass

--- assets/js/player.js
+++ assets/js/player.js
@@ -1,34 +1,35 @@
 $(function () {
   jwplayer('video').setup({
-    file: 'http://www.youtube.com/watch?v=jI-kpVh6e1U',
-    width: '480',
-    height: '270'
+    "file": 'http://www.youtube.com/watch?v=jI-kpVh6e1U',
+    "width": '480',
+    "height": '270',
+    'provider': 'youtube',
+    'plugins': {
+      'hd-2': {}
+    }
   });
 
   // master player
   var player = jwplayer(0);
 
+  var seek_time = 8 * 60 * 60;
+
   // obslugujemy event przed rozpoczeciem odtwarzania
   player.onBeforePlay(function () {
-    // console.log(arguments);
-
-    console.log({
-      total_time: this.getDuration(),
-      total_time2: player.getDuration(),
-      total_time3: jwplayer(0).getDuration()
-    });
-
-
-    player.seek(8 * 60 * 60);
-
+    // seekujemy
+    console.log("[player] seek", seek_time, "s");
+    player.seek(seek_time);
   });
 
   // uruchamiamy pplayer
   player.play(true);
+  console.log("[player] play");
 
   // wylaczamy kontrolki
   player.setControls(false);
+  console.log("[player] controls: hide");
 
   // max glosnika
-  player.setVolume(100);
+  player.setVolume(0);
+  console.log("[player] volume: max");
 });
