bc6474e09c0f5300688252adede3e04875053b11
piecioshka <piecioshka@gmail.com>
2012-11-10 07:23:13 +0100

    troche dodatkow

--- assets/js/player.js
+++ assets/js/player.js
@@ -1,10 +1,34 @@
 $(function () {
-  // master player
-  var player = jwplayer('video');
-
-  player.setup({
-    file: 'http://www.youtube.com/watch?v=c0WcHbXUVVM',
+  jwplayer('video').setup({
+    file: 'http://www.youtube.com/watch?v=jI-kpVh6e1U',
     width: '480',
     height: '270'
   });
+
+  // master player
+  var player = jwplayer(0);
+
+  // obslugujemy event przed rozpoczeciem odtwarzania
+  player.onBeforePlay(function () {
+    // console.log(arguments);
+
+    console.log({
+      total_time: this.getDuration(),
+      total_time2: player.getDuration(),
+      total_time3: jwplayer(0).getDuration()
+    });
+
+
+    player.seek(8 * 60 * 60);
+
+  });
+
+  // uruchamiamy pplayer
+  player.play(true);
+
+  // wylaczamy kontrolki
+  player.setControls(false);
+
+  // max glosnika
+  player.setVolume(100);
 });
