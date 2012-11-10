81b22b5d1c71e178e6a6852fdeb6e5160959c61c
piecioshka <piecioshka@gmail.com>
2012-11-10 08:26:30 +0100

    random seek

--- assets/js/player.js
+++ assets/js/player.js
@@ -1,29 +1,22 @@
 $(function () {
+
+  function get_percent(total_seconds, percent) {
+    return total_seconds * percent / 100;
+  }
+
+  function get_rand_progress(from, to) {
+    return (Math.random() * (to - from) + from).toFixed(0);
+  }
+
   jwplayer('video').setup({
-    "file": 'http://www.youtube.com/watch?v=jI-kpVh6e1U',
+    "file": 'http://www.youtube.com/watch?v=6kw1UVovByw',
     "width": '480',
-    "height": '270',
-    'provider': 'youtube',
-    'plugins': {
-      'hd-2': {}
-    }
+    "height": '270'
   });
 
-  // master player
   var player = jwplayer(0);
+  player.timeout_to_get_total_time = false;
 
-  var seek_time = 8 * 60 * 60;
-
-  // obslugujemy event przed rozpoczeciem odtwarzania
-  player.onBeforePlay(function () {
-    // seekujemy
-    console.log("[player] seek", seek_time, "s");
-    player.seek(seek_time);
-  });
-
-  // uruchamiamy pplayer
-  player.play(true);
-  console.log("[player] play");
 
   // wylaczamy kontrolki
   player.setControls(false);
@@ -32,4 +25,26 @@ $(function () {
   // max glosnika
   player.setVolume(0);
   console.log("[player] volume: max");
+
+  // uruchamiamy pplayer
+  player.play(true);
+  console.log("[player] play");
+
+  player.onPlay(function () {
+    if (!player.timeout_to_get_total_time) {
+      // zatrzymujemy
+      player.stop();
+
+      setTimeout(function () {
+        // seekujemy do losowej wartosci
+        var rand = get_rand_progress(5, 50);
+        player.seek(get_percent(player.getDuration(), rand));
+        console.log("[player] seekujemy do ", rand);
+
+        // odtwarzamy z juz przeseekowanym materialem
+        player.play(true);
+      }, 1000);
+      player.timeout_to_get_total_time = true;
+    }
+  });
 });
