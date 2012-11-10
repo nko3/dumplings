cdae500a8b7575af9ef211f21613ae343d8c2efd
Kamil Zaleski <kamil.zaleski@gmail.com>
2012-11-10 05:37:14 +0100

    some scaffold

---
+++ assets/js/app.js
@@ -0,0 +1,4 @@
+//= require lib/jquery-1.8.2.js
+//= require lib/bootstrap.js
+
+
--- server.js
+++ server.js
@@ -21,6 +21,7 @@ app.configure(function(){
   app.use(express.methodOverride());
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
+  app.use(require('connect-assets')());
 });
 
 app.configure('development', function(){
