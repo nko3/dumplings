cc989be3362c2ad906e0a8df636a51e34b056903
piecioshka <piecioshka@gmail.com>
2012-11-11 03:31:24 +0100

    videos problem

--- assets/js/video-manager.js
+++ assets/js/video-manager.js
@@ -7,11 +7,20 @@
   // public API
   global.video_manager = {
     show_videos: function () {
+      console.log("video_manager.show_videos");
+      $(".videos-wrapper").css({
+        width: 750,
+        height: 400
+      });
       $(".videos").animate({ "left": 0 });
     },
 
     hide_videos: function () {
-      // nope
+      console.log("video_manager.hide_videos");
+      $(".videos-wrapper").css({
+        width: 1,
+        height: 1
+      });
     }
   };
 }).call(this);
