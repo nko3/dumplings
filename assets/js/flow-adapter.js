f7a374bffaca04f8a369d8930dee8c285521118e
piecioshka <piecioshka@gmail.com>
2012-11-11 01:45:28 +0100

    fix socket listener

--- assets/js/flow-adapter.js
+++ assets/js/flow-adapter.js
@@ -33,15 +33,6 @@
 
       pklib.cookie.create("user_name", name);
 
-      socket.on('player-create', function (data) {
-        console.log('[game] ✓ player created', data.id);
-
-        pklib.cookie.create("user_id", data.id);
-
-        // spradzamy czy user wchodzi z linku
-        flow_adapter._game_flow();
-      });
-
       socket.emit("player-create", name);
       console.log("COMMAND: player-create: " + name);
     },
@@ -49,73 +40,12 @@
     _game_flow: function () {
       if (game_link_exists()) {
         // TAK
-        socket.on('game-ready', function (players) {
-          console.log('[game] ✓ game-ready', players);
-
-          // pokazuje VERSUS
-          screen_manager.show_screen("screen-versus");
-          versus.init();
-        });
-
-        socket.on('game-start', function (data) {
-          console.log('[game] ✓ game-start', data);
-
-          trailer.List = data.movies;
-
-          for (var i = 0; i < trailer.List.length; ++i) {
-            trailer.List[i].duration = 3 * 60;
-          }
-
-          screen_manager.show_screen("screen-game");
-
-          var player_manager = new trailer.MovieManager();
-
-          // stworz playery
-          player_manager.create_movies(5);
-          player_manager.load_all_movies_buffer(function () {
-            console.log("✓ All movies loaded");
-
-            video_manager.show_videos();
-            answer_manager.show_answers();
-
-            process_indicator.show_play_bar();
-            process_indicator.hide_progress_bar();
-
-            player_manager.play_queue(function () {
-              console.log("✓ All movies played!");
-
-              video_manager.hide_videos();
-              answer_manager.hide_answers();
-
-              screen_manager.show_screen("screen-thanks");
-              process_indicator.hide_play_bar();
-
-            }, player_manager._movies);
-          });
-        });
 
         var game_id = get_hash_params("game");
         socket.emit("game-join", game_id);
         console.log("COMMAND game-join", game_id);
       } else {
         // NIE
-        socket.on('game-create', function (id) {
-          console.log('[game] ✓ game created id', id);
-
-          screen_manager.show_screen("screen-send-link");
-
-          var protocol = document.location.protocol;
-          var host = document.location.host;
-          var pathname = document.location.pathname;
-          var game_url = "/game=" + id;
-
-          $(".send-link").val(protocol + "//" + host + pathname + "#" + game_url);
-
-          // NEXT STEP
-          // document.location.hash = game_url;
-
-          sent_link.init();
-        });
 
         socket.emit('game-create');
         console.log("COMMAND: game-create");
--- assets/js/game.js
+++ assets/js/game.js
@@ -15,16 +15,79 @@
     });
 
     socket.on('connect', function () {
-      console.log('[game] connected');
+      console.log('[game] ✓ connected');
+    });
+    socket.on('game-ready', function (players) {
+      console.log('[game] ✓ game-ready', players);
+
+      // pokazuje VERSUS
+      screen_manager.show_screen("screen-versus");
+      versus.init();
     });
 
-    // socket.emit('player-create', 'CHOSEN_NAME');
+    socket.on('game-start', function (data) {
+      console.log('[game] ✓ game-start', data);
 
-    // socket.emit('game-create')
+      trailer.List = data.movies;
 
-    // socket.emit('game-join',id)
+      for (var i = 0; i < trailer.List.length; ++i) {
+        trailer.List[i].duration = 3 * 60;
+      }
 
-    // socket.emit('game-start',id)
+      screen_manager.show_screen("screen-game");
+
+      var player_manager = new trailer.MovieManager();
+
+      // stworz playery
+      player_manager.create_movies(5);
+      player_manager.load_all_movies_buffer(function () {
+        console.log("✓ All movies loaded");
+
+        video_manager.show_videos();
+        answer_manager.show_answers();
+
+        process_indicator.show_play_bar();
+        process_indicator.hide_progress_bar();
+
+        player_manager.play_queue(function () {
+          console.log("✓ All movies played!");
+
+          video_manager.hide_videos();
+          answer_manager.hide_answers();
+
+          screen_manager.show_screen("screen-thanks");
+          process_indicator.hide_play_bar();
+
+        }, player_manager._movies);
+      });
+    });
+
+    socket.on('game-create', function (id) {
+      console.log('[game] ✓ game created id', id);
+
+      screen_manager.show_screen("screen-send-link");
+
+      var protocol = document.location.protocol;
+      var host = document.location.host;
+      var pathname = document.location.pathname;
+      var game_url = "/game=" + id;
+
+      $(".send-link").val(protocol + "//" + host + pathname + "#" + game_url);
+
+      // NEXT STEP
+      // document.location.hash = game_url;
+
+      sent_link.init();
+    });
+
+    socket.on('player-create', function (data) {
+      console.log('[game] ✓ player created', data.id);
+
+      pklib.cookie.create("user_id", data.id);
+
+      // spradzamy czy user wchodzi z linku
+      flow_adapter._game_flow();
+    });
 
     // socket.emit('game-answer', game_id, movie_id, answer_id, time);
     socket.on('game-answer', function (game_id, movie_id, answer_id, time, player_id, correct) {
