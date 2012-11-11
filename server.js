507d7451c982a3fb11a867cef1a89f13042d5ce2
Kamil Zaleski <kamil.zaleski@gmail.com>
2012-11-11 22:29:34 +0100

    logger & title

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.favicon());
-  app.use(express.logger('dev'));
+  app.use(express.logger());
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
@@ -115,6 +115,7 @@ function randMovies(cb) {
           id: movie.yt,
           url: "http://www.youtube.com/watch?v=" + movie.yt,
           duration: movie.du,
+          title: movie.name,
           answers: [{
             id: movie.id,
             title: movie.na
