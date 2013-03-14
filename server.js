240080848cdbfdccd3844825a4acf8076c100e46
Kamil Zaleski <kamil.zaleski@gmail.com>
2013-03-14 21:27:22 +0100

    port for socket.io

--- server.js
+++ server.js
@@ -567,14 +567,6 @@ io.on('connection', function(socket) {
 });
 
 app.get('/', function(req, res){
-  var socket;
-
-  if (app.get('env') == 'production') {
-    socket = 'http://dumplings.nko3.jit.su/';
-  } else {
-    socket = 'http://localhost:3000/';
-  }
-
-  res.render('index', { title: 'Trailer Challenge', config: { socket: socket, env: app.get('env') } });
+  res.render('index', { title: 'Trailer Challenge', config: { appPort: app.get('port'), env: app.get('env') } });
 });
 
